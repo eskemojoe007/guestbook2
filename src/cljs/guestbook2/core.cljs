bc3dd9b7558ee1644fc14d0e46ade8e26566556f
eskemojoe007 <David.Folkner@gmail.com>
2019-07-25 13:02:14 -0400

    Temp Re-create Re-Frame
    This is early in the book, but got it to work.

--- src/cljs/guestbook2/core.cljs
+++ src/cljs/guestbook2/core.cljs
@@ -1,88 +1,268 @@
 (ns guestbook2.core
   (:require
-    [day8.re-frame.http-fx]
-    [reagent.core :as r]
-    [re-frame.core :as rf]
-    [goog.events :as events]
-    [goog.history.EventType :as HistoryEventType]
-    [markdown.core :refer [md->html]]
-    [guestbook2.ajax :as ajax]
-    [guestbook2.events]
-    [reitit.core :as reitit]
-    [clojure.string :as string])
-  (:import goog.History))
+   [reagent.core :as r]
+   [re-frame.core :as rf]
+   [ajax.core :refer [GET POST]]
+   [clojure.string :as string]
+   [guestbook2.validation :refer [validate-message]]))
 
-(defn nav-link [uri title page]
-  [:a.navbar-item
-   {:href   uri
-    :class (when (= page @(rf/subscribe [:page])) :is-active)}
-   title])
 
-(defn navbar []
-  (r/with-let [expanded? (r/atom false)]
-    [:nav.navbar.is-info>div.container
-     [:div.navbar-brand
-      [:a.navbar-item {:href "/" :style {:font-weight :bold}} "guestbook2"]
-      [:span.navbar-burger.burger
-       {:data-target :nav-menu
-        :on-click #(swap! expanded? not)
-        :class (when @expanded? :is-active)}
-       [:span][:span][:span]]]
-     [:div#nav-menu.navbar-menu
-      {:class (when @expanded? :is-active)}
-      [:div.navbar-start
-       [nav-link "#/" "Home" :home]
-       [nav-link "#/about" "About" :about]]]]))
+; (-> (.getElementById js/document "content")
+;     (.-innerHTML)
+;     (set! "Hello, World!"))
+; (r/render
+;  [:div#hello.content>h1 "Hello, Auto!"]
+;  (.getElementById js/document "content"))
 
-(defn about-page []
-  [:section.section>div.container>div.content
-   [:img {:src "/img/warning_clojure.png"}]])
+;;;; re-frame functions
 
-(defn home-page []
-  [:section.section>div.container>div.content
-   (when-let [docs @(rf/subscribe [:docs])]
-     [:div {:dangerouslySetInnerHTML {:__html (md->html docs)}}])])
 
-(def pages
-  {:home #'home-page
-   :about #'about-page})
+(rf/reg-event-fx
+ :app/initialize
+ (fn [_ _]
+   {:db {:messages/loading? true}}))
 
-(defn page []
-  [:div
-   [navbar]
-   [(pages @(rf/subscribe [:page]))]])
+(rf/reg-sub
+ :messages/loading?
+ (fn [db _]
+   (:messages/loading? db)))
 
-;; -------------------------
-;; Routes
+(rf/reg-event-db
+ :messages/set
+ (fn [db [_ messages]]
+   (-> db
+       (assoc :messages/loading? false
+              :messages/list messages))))
 
-(def router
-  (reitit/router
-    [["/" :home]
-     ["/about" :about]]))
+(rf/reg-sub
+ :messages/list
+ (fn [db _]
+   (:messages/list db [])))
 
-;; -------------------------
-;; History
-;; must be called after routes have been defined
-(defn hook-browser-navigation! []
-  (doto (History.)
-    (events/listen
-      HistoryEventType/NAVIGATE
-      (fn [event]
-        (let [uri (or (not-empty (string/replace (.-token event) #"^.*#" "")) "/")]
-          (rf/dispatch
-            [:navigate (reitit/match-by-path router uri)]))))
-    (.setEnabled true)))
+(rf/reg-event-db
+ :message/add
+ (fn [db [_ message]]
+   (update db :messages/list conj message)))
 
-;; -------------------------
-;; Initialize app
-(defn mount-components []
-  (rf/clear-subscription-cache!)
-  (r/render [#'page] (.getElementById js/document "app")))
 
-(defn init! []
-  (rf/dispatch-sync [:navigate (reitit/match-by-name router :home)])
 
-  (ajax/load-interceptors!)
-  (rf/dispatch [:fetch-docs])
-  (hook-browser-navigation!)
+;;;; Reagent Functions
+
+
+(defn send-message!
+  "Sends form details to api backend."
+  [fields errors]
+  (if-let [validation-errors (validate-message @fields)]
+    (reset! errors validation-errors)
+    (POST "/api/message"
+          {:format :json
+           :headers
+           {"Accept" "application/transit+json"
+            "x-csrf-token" (.-value (.getElementById js/document "token"))}
+           :params @fields
+           :handler #(do
+                       ; (.log js/console (str @fields))
+                       ; (.log js.console (str @messages))
+                       ; (.log js/console (str (conj @messages (assoc @fields :timestamp (js/Date.)))))
+                       ; (swap! messages conj (assoc @fields :timestamp (js/Date.)))
+                       (rf/dispatch [:message/add (-> @fields
+                                                      (assoc :timestamp (js/Date.))
+                                                      (update :name str " [CLIENT]"))])
+                       (reset! errors nil)
+                       (reset! fields nil))
+           :error-handler #(do
+                             (.log js.console (str %))
+                             (reset! errors (get-in % [:response :errors])))})))
+
+(defn get-messages
+  "Gets messages from api and dispatches messages"
+  []
+  (GET "/api/messages"
+       {:headers {"Accept" "application/transit+json"}
+        :handler #(do
+                    ; (.log js/console (str (:messages %)))
+                    (rf/dispatch [:messages/set (:messages %)]))}))
+
+(defn errors-component
+  "React component based on an errors atom.
+  errors - r/atom that is used to store our errors.
+  id - the key used to extract the kind of error we have"
+  [errors id]
+  (when-let [error (id @errors)]
+    [:div.notification.is-danger (string/join error)]))
+
+; (defn message-list-component
+;   [message]
+;   [:li
+;    [:time (:timestamp message)]
+;    [:p (:message message)]
+;    [:p (str " - " (:name message))]])
+
+(defn message-form
+  "Component that is a message form"
+  []
+  (let [fields (r/atom {})
+        errors (r/atom {})]
+    (fn []
+      [:div
+       [errors-component errors :server-error]
+       [:div.field
+        [:label.label {:for :name} "Name"]
+        [errors-component errors :name]
+        [:input.input
+         {:type :text
+          :name :name
+          :on-change #(swap! fields assoc :name (-> % .-target .-value))
+          :value (:name @fields)}]]
+       [:div.field
+        [:label.label {:for :message} "Message"]
+        [errors-component errors :message]
+        [:textarea.textarea
+         {:type :text
+          :name :message
+          :on-change #(swap! fields assoc :message (-> % .-target .-value))
+          :value (:message @fields)}]]
+       [:input.button.is-primary
+        {:type :submit
+         :on-click #(send-message! fields errors)
+         :value "comment"}]
+       [:p "Name: " (:name @fields)]
+       [:p "Message: " (:message @fields)]])))
+
+(defn message-list
+  [messages]
+  [:ul.messages
+   ; (for [{:keys [timestamp message name]} (sort-by :timestamp #(compare %2 %1) @messages)])
+   (for [{:keys [timestamp message name]} (reverse @messages)]
+     ^{:key timestamp}
+     [:li
+      [:time (.toLocaleString timestamp)]
+      [:p message]
+      [:p "@" name]])])
+
+(defn home []
+  (let [messages (rf/subscribe [:messages/list])]
+    ; (rf/dispatch [:app/initialize])
+    ; (get-messages)
+    (fn []
+      (if @(rf/subscribe [:messages/loading?])
+        [:div>div.row>div.span12>h3 "Loading Messages..."]
+        [:div.content
+         [:div.columns.is-centered>div.column.is-two-thirds
+          [:div.columns>div.column
+           [message-form]]
+          [:div.columns>div.column
+           [:h3 "Messages"]
+           [message-list messages]]]]))))
+
+(defn mount-components
+  []
+  (.log js.console "Mounting Components...")
+  (r/render [#'home] (.getElementById js/document "app"))
+  (.log js.console "Components Mounted!"))
+
+(defn init!
+  []
+  (.log js.console "Initializing App..")
+  (rf/dispatch [:app/initialize])
+  (get-messages)
   (mount-components))
+; (.log js/console "guestbook.core evaluated!")
+; (.log js/console "guestbook.core evaluated 2!")
+;
+; (r/render
+;   [home]
+;   (.getElementById js/document "content"))
+
+; (ns guestbook2.core
+;   (:require
+;     [day8.re-frame.http-fx]
+;     [reagent.core :as r]
+;     [re-frame.core :as rf]
+;     [goog.events :as events]
+;     [goog.history.EventType :as HistoryEventType]
+;     [markdown.core :refer [md->html]]
+;     [guestbook2.ajax :as ajax]
+;     [guestbook2.events]
+;     ; [ajax.core :refer [GET POST]]
+;     [reitit.core :as reitit]
+;     [clojure.string :as string]
+;     [guestbook2.validation :refer [validate-message]])
+;   (:import goog.History))
+;
+;
+;
+; (defn nav-link [uri title page]
+;   [:a.navbar-item
+;    {:href   uri
+;     :class (when (= page @(rf/subscribe [:page])) :is-active)}
+;    title])
+;
+; (defn navbar []
+;   (r/with-let [expanded? (r/atom false)]
+;     [:nav.navbar.is-info>div.container
+;      [:div.navbar-brand
+;       [:a.navbar-item {:href "/" :style {:font-weight :bold}} "guestbook2"]
+;       [:span.navbar-burger.burger
+;        {:data-target :nav-menu
+;         :on-click #(swap! expanded? not)
+;         :class (when @expanded? :is-active)}
+;        [:span][:span][:span]]]
+;      [:div#nav-menu.navbar-menu
+;       {:class (when @expanded? :is-active)}
+;       [:div.navbar-start
+;        [nav-link "#/" "Home" :home]
+;        [nav-link "#/about" "About" :about]]]]))
+;
+; (defn about-page []
+;   [:section.section>div.container>div.content
+;    [:img {:src "/img/warning_clojure.png"}]])
+;
+; (defn home-page []
+;   [:section.section>div.container>div.content
+;    (when-let [docs @(rf/subscribe [:docs])]
+;      [:div {:dangerouslySetInnerHTML {:__html (md->html docs)}}])])
+;
+; (def pages
+;   {:home #'home-page
+;    :about #'about-page})
+;
+; (defn page []
+;   [:div
+;    [navbar]
+;    [(pages @(rf/subscribe [:page]))]])
+;
+; ;; -------------------------
+; ;; Routes
+;
+; (def router
+;   (reitit/router
+;     [["/" :home]
+;      ["/about" :about]]))
+;
+; ;; -------------------------
+; ;; History
+; ;; must be called after routes have been defined
+; (defn hook-browser-navigation! []
+;   (doto (History.)
+;     (events/listen
+;       HistoryEventType/NAVIGATE
+;       (fn [event]
+;         (let [uri (or (not-empty (string/replace (.-token event) #"^.*#" "")) "/")]
+;           (rf/dispatch
+;             [:navigate (reitit/match-by-path router uri)]))))
+;     (.setEnabled true)))
+;
+; ;; -------------------------
+; ;; Initialize app
+; (defn mount-components []
+;   (rf/clear-subscription-cache!)
+;   (r/render [#'page] (.getElementById js/document "app")))
+;
+; (defn init! []
+;   (rf/dispatch-sync [:navigate (reitit/match-by-name router :home)])
+;
+;   (ajax/load-interceptors!)
+;   (rf/dispatch [:fetch-docs])
+;   (hook-browser-navigation!)
+;   (mount-components))
